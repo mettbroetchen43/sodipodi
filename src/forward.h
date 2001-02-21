14db82310143672b677c9bc29197813ab7f0b47e
Lauris Kaplinski <lauris@src.gnome.org>
2001-02-21 18:31:44 +0000

    Commented out nonworking GNOME_PRINT_CHECK. Added forward.h to CVS.

---
+++ src/forward.h
@@ -0,0 +1,55 @@
+#ifndef _FORWARD_H_
+#define _FORWARD_H_
+
+/*
+ * Forward declarations of most used objects
+ *
+ * Author: Lauris Kaplinski <lauris@ximian.com>
+ *
+ * Copyright (C) 2001 Ximian, Inc.
+ *
+ */
+
+typedef struct _SPDocument SPDocument;
+typedef struct _SPDocumentClass SPDocumentClass;
+
+typedef struct _SPDesktop SPDesktop;
+typedef struct _SPDesktopClass SPDesktopClass;
+
+typedef struct _SPSelection SPSelection;
+typedef struct _SPSelectionClass SPSelectionClass;
+
+typedef struct _SPEventContext SPEventContext;
+typedef struct _SPEventContextClass SPEventContextClass;
+
+typedef struct _SPObject SPObject;
+typedef struct _SPObjectClass SPObjectClass;
+
+typedef struct _SPItem SPItem;
+typedef struct _SPItemClass SPItemClass;
+
+typedef struct _SPGroup SPGroup;
+typedef struct _SPGroupClass SPGroupClass;
+
+typedef struct _SPRoot SPRoot;
+typedef struct _SPRootClass SPRootClass;
+
+typedef struct _SPNamedView SPNamedView;
+typedef struct _SPNamedViewClass SPNamedViewClass;
+
+typedef struct _SPGuide SPGuide;
+typedef struct _SPGuideClass SPGuideClass;
+
+typedef struct _SPObjectGroup SPObjectGroup;
+typedef struct _SPObjectGroupClass SPObjectGroupClass;
+
+typedef struct _SPPath SPPath;
+typedef struct _SPPathClass SPPathClass;
+
+typedef struct _SPShape SPShape;
+typedef struct _SPShapeClass SPShapeClass;
+
+typedef struct _SPChars SPChars;
+typedef struct _SPCharsClass SPCharsClass;
+
+#endif
