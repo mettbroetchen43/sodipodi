a8ba31fd703fa098dac3cf1dd188596d2486c272
Lauris Kaplinski <lauris@src.gnome.org>
2001-05-07 17:29:18 +0000

    Added two missing files from display/nr

---
+++ src/display/nr/sp-canvas.h
@@ -0,0 +1,45 @@
+#ifndef __SP_ARENA_H__
+#define __SP_ARENA_H__
+
+/*
+ * SPArena
+ *
+ * Antialiased drawing engine for Sodipodi
+ *
+ * Author: Lauris Kaplinski <lauris@ximian.com>
+ * Copyright (C) 2000-2001 Ximian, Inc.
+ *
+ */
+
+#include <libgnome/gnome-defs.h>
+
+BEGIN_GNOME_DECLS
+
+typedef struct _SPArena SPArena;
+typedef struct _SPArenaClass SPArenaClass;
+
+#define SP_TYPE_ARENA (sp_arena_get_type ())
+#define SP_ARENA(o) (GTK_CHECK_CAST ((o), SP_TYPE_ARENA, SPArena))
+#define SP_ARENA_CLASS(k) (GTK_CHECK_CLASS_CAST ((k), SP_TYPE_ARENA, SPArenaClass))
+#define SP_IS_ARENA(o) (GTK_CHECK_TYPE ((o), SP_TYPE_ARENA))
+#define SP_IS_ARENA_CLASS(k) (GTK_CHECK_CLASS_TYPE ((k), SP_TYPE_ARENA))
+
+#include <gtk/gtktypeutil.h>
+#include "sp-arena-item.h"
+
+struct _SPArena {
+	GtkObject object;
+	SPArenaItem * root;
+};
+
+struct _SPArenaClass {
+	GtkObjectClass object_class;
+};
+
+GtkType sp_arena_get_type (void);
+
+SPArena * sp_arena_new (void);
+
+END_GNOME_DECLS
+
+#endif
