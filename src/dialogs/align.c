73ee2437224de18b49897e017e6b179000659681
Lauris Kaplinski <lauris@src.gnome.org>
2002-10-23 20:27:02 +0000

    Ported align dialog from glade to plain Gtk+

--- src/dialogs/align.c
+++ src/dialogs/align.c
@@ -1,9 +1,10 @@
 #define __SP_QUICK_ALIGN_C__
 
 /*
- * PNG export dialog
+ * Object align dialog
  *
  * Authors:
+ *   Frank Felfe <innerspace@iname.com>
  *   Lauris Kaplinski <lauris@kaplinski.com>
  *
  * Copyright (C) 1999-2002 Authors
@@ -12,14 +13,17 @@
  */
 
 #include <config.h>
+
 #include <math.h>
 #include <glib.h>
-#include <gtk/gtksignal.h>
-#include <gtk/gtkoptionmenu.h>
-#include <gtk/gtkmenuitem.h>
 #include <libgnome/gnome-defs.h>
 #include <libgnome/gnome-i18n.h>
-#include <glade/glade.h>
+#include <gtk/gtksignal.h>
+#include <gtk/gtkwindow.h>
+#include <gtk/gtktable.h>
+#include <gtk/gtkoptionmenu.h>
+#include <gtk/gtkmenuitem.h>
+#include <libgnomeui/gnome-stock.h>
 #include "../sodipodi.h"
 #include "../document.h"
 #include "../desktop-handles.h"
@@ -32,12 +36,11 @@
  *
  * todo: dialog with more aligns
  * - more aligns (30 % left from center ...)
- * - new align types (align to smaler object / chose objects to align to)
  * - aligns for nodes
  *
  */ 
 
-typedef enum {
+enum {
 	SP_ALIGN_LAST,
 	SP_ALIGN_FIRST,
 	SP_ALIGN_BIGGEST,
@@ -45,7 +48,20 @@ typedef enum {
 	SP_ALIGN_PAGE,
 	SP_ALIGN_DRAWING,
 	SP_ALIGN_SELECTION,
-} SPAlignBase;
+};
+
+void sp_quick_align_top_in (void);
+void sp_quick_align_top_out (void);
+void sp_quick_align_bottom_in (void);
+void sp_quick_align_bottom_out (void);
+void sp_quick_align_left_in (void);
+void sp_quick_align_left_out (void);
+void sp_quick_align_right_in (void);
+void sp_quick_align_right_out (void);
+void sp_quick_align_center_hor (void);
+void sp_quick_align_center_ver (void);
+
+void sp_quick_align_dialog_close (void);
 
 static void sp_quick_align_arrange (gdouble mx0, gdouble mx1, gdouble my0, gdouble my1,
 				    gdouble sx0, gdouble sx1, gdouble sy0, gdouble sy1);
@@ -53,37 +69,79 @@ static GtkWidget * create_base_menu (void);
 static void set_base (GtkMenuItem * menuitem, gpointer data);
 static SPItem * sp_quick_align_find_master (const GSList * slist, gboolean horizontal);
 
-static GladeXML  * quick_align_xml = NULL;
-static GtkWidget * quick_align_dialog = NULL;
-static SPAlignBase base = SP_ALIGN_LAST;
+static GtkWidget *dlg = NULL;
+
+static unsigned int base = SP_ALIGN_LAST;
+
+static int
+sp_quick_align_dialog_delete (void)
+{
+	if (GTK_WIDGET_VISIBLE (dlg)) gtk_widget_hide (dlg);
+
+	return TRUE;
+}
+
+static void
+sp_align_add_button (GtkWidget *t, int col, int row, GtkSignalFunc handler, const unsigned char *pxname)
+{
+	GtkWidget *pm, *b;
+	unsigned char c[1024];
+
+	g_snprintf (c, 1024, "%s/%s", SODIPODI_PIXMAPDIR, pxname);
+	pm = gnome_stock_pixmap_widget (t, c);
+	gtk_widget_show (pm);
+	b = gtk_button_new ();
+	gtk_widget_show (b);
+	gtk_container_add (GTK_CONTAINER (b), pm);
+	if (handler) gtk_signal_connect (GTK_OBJECT (b), "clicked", handler, NULL);
+	gtk_table_attach (GTK_TABLE (t), b, col, col + 1, row, row + 1, 0, 0, 0, 0);
+}
 
 void
 sp_quick_align_dialog (void)
 {
-	if (quick_align_dialog == NULL) {
-		GtkWidget * align_base;
+	if (!dlg) {
+		GtkWidget *vb, *om, *t;
 
-		quick_align_xml = glade_xml_new (SODIPODI_GLADEDIR "/align.glade", "quick_align");
-		if (quick_align_xml == NULL) return;
-		glade_xml_signal_autoconnect (quick_align_xml);
-		quick_align_dialog = glade_xml_get_widget (quick_align_xml, "quick_align");
-		if (quick_align_dialog == NULL) return;
-		align_base = glade_xml_get_widget (quick_align_xml, "align_base");
-		if (align_base == NULL) return;
-		gtk_option_menu_set_menu ((GtkOptionMenu *) align_base, create_base_menu ());
-	} else {
-		if (!GTK_WIDGET_VISIBLE (quick_align_dialog))
-			gtk_widget_show (quick_align_dialog);
+		dlg = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+		gtk_window_set_title (GTK_WINDOW (dlg), _("Align objects"));
+		gtk_signal_connect (GTK_OBJECT (dlg), "delete_event", GTK_SIGNAL_FUNC (sp_quick_align_dialog_delete), NULL);
+
+		vb = gtk_vbox_new (FALSE, 0);
+		gtk_widget_show (vb);
+		gtk_container_add (GTK_CONTAINER (dlg), vb);
+
+		om = gtk_option_menu_new ();
+		gtk_widget_show (om);
+		gtk_box_pack_start (GTK_BOX (vb), om, FALSE, FALSE, 0);
+		gtk_option_menu_set_menu (GTK_OPTION_MENU (om), create_base_menu ());
+
+		t = gtk_table_new (2, 5, TRUE);
+		gtk_widget_show (t);
+		gtk_box_pack_start (GTK_BOX (vb), t, FALSE, FALSE, 0);
+
+		sp_align_add_button (t, 0, 0, GTK_SIGNAL_FUNC (sp_quick_align_left_out), "al_left_out.xpm");
+		sp_align_add_button (t, 1, 0, GTK_SIGNAL_FUNC (sp_quick_align_left_in), "al_left_in.xpm");
+		sp_align_add_button (t, 2, 0, GTK_SIGNAL_FUNC (sp_quick_align_center_hor), "al_center_hor.xpm");
+		sp_align_add_button (t, 3, 0, GTK_SIGNAL_FUNC (sp_quick_align_right_in), "al_right_in.xpm");
+		sp_align_add_button (t, 4, 0, GTK_SIGNAL_FUNC (sp_quick_align_right_out), "al_right_out.xpm");
+
+		sp_align_add_button (t, 0, 1, GTK_SIGNAL_FUNC (sp_quick_align_top_out), "al_top_out.xpm");
+		sp_align_add_button (t, 1, 1, GTK_SIGNAL_FUNC (sp_quick_align_top_in), "al_top_in.xpm");
+		sp_align_add_button (t, 2, 1, GTK_SIGNAL_FUNC (sp_quick_align_center_ver), "al_center_ver.xpm");
+		sp_align_add_button (t, 3, 1, GTK_SIGNAL_FUNC (sp_quick_align_bottom_in), "al_bottom_in.xpm");
+		sp_align_add_button (t, 4, 1, GTK_SIGNAL_FUNC (sp_quick_align_bottom_out), "al_bottom_out.xpm");
 	}
+
+	if (!GTK_WIDGET_VISIBLE (dlg)) gtk_widget_show (dlg);
 }
 
 void
 sp_quick_align_dialog_close (void)
 {
-	g_assert (quick_align_dialog != NULL);
+	g_assert (dlg != NULL);
 
-	if (GTK_WIDGET_VISIBLE (quick_align_dialog))
-		gtk_widget_hide (quick_align_dialog);
+	if (GTK_WIDGET_VISIBLE (dlg)) gtk_widget_hide (dlg);
 }
 
 static GtkWidget *
@@ -136,9 +194,9 @@ create_base_menu (void)
 }
 
 static void
-set_base (GtkMenuItem * menuitem, gpointer data)
+set_base (GtkMenuItem *menuitem, gpointer data)
 {
-	base = (SPAlignBase) data;
+	base = GPOINTER_TO_UINT (data);
 }
 
 
@@ -278,7 +336,7 @@ sp_quick_align_arrange (gdouble mx0, gdouble mx1, gdouble my0, gdouble my1,
 }
 
 static SPItem *
-sp_quick_align_find_master (const GSList * slist, gboolean horizontal)
+sp_quick_align_find_master (const GSList *slist, gboolean horizontal)
 {
 	ArtDRect b;
 	const GSList * l;
