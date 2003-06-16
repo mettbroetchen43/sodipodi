49d375f5c09440436104735724978be3bdf634c3
Lauris Kaplinski <lauris@src.gnome.org>
2003-06-16 19:15:29 +0000

    Fixed typo in window icon setting

--- src/main.c
+++ src/main.c
@@ -74,7 +74,7 @@
 #ifndef HAVE_BIND_TEXTDOMAIN_CODESET
 #define bind_textdomain_codeset(p,c)
 #endif
-#ifndef GAVE_GTK_WINDOW_SET_DEFAULT_ICON_FROM_FILE
+#ifndef HAVE_GTK_WINDOW_SET_DEFAULT_ICON_FROM_FILE
 #define gtk_window_set_default_icon_from_file(f,v)
 #endif
 
