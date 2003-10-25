c0b8ad971c0e12b4aa58f52b4e76caa2e17e5ac0
Mitsuru Oka <oka326@src.gnome.org>
2003-10-25 11:04:13 +0000

    Fixed to work with GnomePrint

--- src/libnrtype/nr-type-gnome.c
+++ src/libnrtype/nr-type-gnome.c
@@ -34,10 +34,10 @@ static void nr_typeface_gnome_font_free (NRFont *font);
 
 static NRTypeFaceClass *parent_class;
 
-NRType
+unsigned int
 nr_typeface_gnome_get_type (void)
 {
-	static NRType type = 0;
+	static unsigned int type = 0;
 	if (!type) {
 		type = nr_object_register_type (NR_TYPE_TYPEFACE,
 						"NRTypeFaceGnome",
--- src/libnrtype/nr-type-gnome.h
+++ src/libnrtype/nr-type-gnome.h
@@ -34,7 +34,7 @@ struct _NRTypeFaceGnomeClass {
 	NRTypeFaceClass typeface_class;
 };
 
-NRType nr_typeface_gnome_get_type (void);
+unsigned int nr_typeface_gnome_get_type (void);
 
 NRNameList *nr_type_gnome_typefaces_get (NRNameList *typefaces);
 NRNameList *nr_type_gnome_families_get (NRNameList *families);
