d17e18d6af37568fd57e0a3e28aeae9e2a2ae837
Mitsuru Oka <oka326@src.gnome.org>
2004-01-09 17:57:22 +0000

    Fixed C-x segv bug

--- src/text-context.c
+++ src/text-context.c
@@ -474,7 +474,7 @@ sp_text_context_selection_changed (SPSelection *selection, SPTextContext *tc)
 
 	item = sp_selection_item (selection);
 
-	if (tc->text && (item != tc->text)) {
+	if (item && tc->text && (item != tc->text)) {
 		sp_text_context_forget_text (tc);
 	}
 	tc->text = NULL;
