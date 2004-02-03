465f730d5fc26ec79aac283a9d872dab589bce65
Lauris Kaplinski <lauris@src.gnome.org>
2004-02-03 18:35:09 +0000

    Added dialogs/xml-editor.c

---
+++ src/dialogs/xml-editor.c
@@ -0,0 +1,17 @@
+#define __SP_XML_EDITOR_C__
+
+/*
+ * XML editor module implementation
+ *
+ * Authors:
+ *   Lauris Kaplinski <lauris@kaplinski.com>
+ *   MenTaLguY <mental@rydia.net>
+ *
+ * Copyright (C) 2000-2003 Authors
+ *
+ * Released under GNU GPL, read the file 'COPYING' for more information
+ *
+ */
+
+#include "xml-tree.h"
+
